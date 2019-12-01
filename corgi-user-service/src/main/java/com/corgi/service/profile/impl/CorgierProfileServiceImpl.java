fddc049ca5e818119d1856d2833de13da62fb741
tairanliu <dasltr@126.com>
2019-12-02 00:56:36 +0800

    init

--- corgi-user-service/src/main/java/com/corgi/service/profile/impl/CorgierProfileServiceImpl.java
+++ corgi-user-service/src/main/java/com/corgi/service/profile/impl/CorgierProfileServiceImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 /**
  * @author tairanliu
  */
-@Service(interfaceClass=CorgierProfileService.class,timeout = 60000,version = "1.0.0")
+@Service(interfaceClass=CorgierProfileService.class,timeout = 60000,version = "${provider.version}")
 @Slf4j
 @Component
 public class CorgierProfileServiceImpl implements CorgierProfileService {
