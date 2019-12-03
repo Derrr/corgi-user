2b7a49c1f254e30f3ae73802429c7c123ed3d815
tairanliu <dasltr@126.com>
2019-12-03 23:19:50 +0800

    init corgi user

--- corgi-user-api/src/main/java/com/corgi/user/entity/UserLogin.java
+++ corgi-user-api/src/main/java/com/corgi/user/entity/UserLogin.java
@@ -2,11 +2,13 @@ package com.corgi.user.entity;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 /**
  * @author tairanliu
  */
 @Data
-public class UserLogin {
+public class UserLogin implements Serializable{
     String userId;
     String telNo;
     String imId;
