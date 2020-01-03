017efb1504fc57cf4c61c6841dd095a06047c6cd
tairanliu <dasltr@126.com>
2020-01-03 16:41:07 +0800

    添加标签相关接口

--- corgi-user-api/src/main/java/com/corgi/user/entity/UserInterest.java
+++ corgi-user-api/src/main/java/com/corgi/user/entity/UserInterest.java
@@ -2,8 +2,13 @@ package com.corgi.user.entity;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
+/**
+ * @author tairanliu
+ */
 @Data
-public class UserInterest {
+public class UserInterest implements Serializable{
     private String userId;
     private String name;
     private String category;
