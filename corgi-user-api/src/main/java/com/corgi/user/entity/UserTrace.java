16c1b58167edec79cf66af64ff29b4fd7791fae0
tairanliu <dasltr@126.com>
2020-02-07 00:27:29 +0800

    fix bug

--- corgi-user-api/src/main/java/com/corgi/user/entity/UserTrace.java
+++ corgi-user-api/src/main/java/com/corgi/user/entity/UserTrace.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 /**
  * @author tairanliu
  */
@@ -12,7 +14,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class UserTrace {
+public class UserTrace implements Serializable{
     public static final String STAYING = "staying";
     public static final String CHANGED = "skipped";
 
