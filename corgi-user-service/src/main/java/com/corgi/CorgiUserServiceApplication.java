e9ea3bc48aa41a10d9c69a9d3e1dd6251104c393
tairanliu <dasltr@126.com>
2020-04-09 15:41:26 +0800

    添加拦截器

--- corgi-user-service/src/main/java/com/corgi/CorgiUserServiceApplication.java
+++ corgi-user-service/src/main/java/com/corgi/CorgiUserServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.amqp.core.Queue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
 /**
@@ -17,6 +18,7 @@ import org.springframework.context.annotation.Bean;
 @MapperScan("com.corgi.mapper")
 @SpringBootApplication
 @EnableDubboConfiguration
+@EnableTransactionManagement
 public class CorgiUserServiceApplication {
 
     public static void main(String[] args) {
