f068bdb9178a1db3f5ece718a07ae04872c57841
tairanliu <dasltr@126.com>
2020-04-26 12:24:05 +0800

    fix bug

--- corgi-user-service/src/main/java/com/corgi/support/UserQuerySupporter.java
+++ corgi-user-service/src/main/java/com/corgi/support/UserQuerySupporter.java
@@ -61,18 +61,19 @@ public class UserQuerySupporter {
         if (!CollectionUtils.isEmpty(userQuery.getRelation())) {
             StringBuilder sb = new StringBuilder("('");
             for (String relation : userQuery.getRelation()) {
-                if(!StringUtils.isEmpty(relation)) {
+                if (!StringUtils.isEmpty(relation)) {
                     sb.append(relation + "','");
                 }
             }
-            sb.append("')");
+            sb.delete(sb.length() - 2, sb.length());
+            sb.append(")");
             this.relation = sb.toString();
         }
 
         if (!CollectionUtils.isEmpty(userQuery.getRole())) {
             StringBuilder sb = new StringBuilder("('");
             for (String role : userQuery.getRole()) {
-                if(!StringUtils.isEmpty(role)) {
+                if (!StringUtils.isEmpty(role)) {
                     sb.append(role + "','");
                 }
             }
@@ -83,7 +84,7 @@ public class UserQuerySupporter {
         if (!CollectionUtils.isEmpty(userQuery.getGroup())) {
             StringBuilder sb = new StringBuilder("('");
             for (String group : userQuery.getGroup()) {
-                if(!StringUtils.isEmpty(group)) {
+                if (!StringUtils.isEmpty(group)) {
                     sb.append(group + "','");
                 }
             }
