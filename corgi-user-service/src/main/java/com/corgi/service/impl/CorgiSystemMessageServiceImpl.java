c1a7c3db500c4803b78d0cb6fec32f556c2a5391
tairanliu <dasltr@126.com>
2020-06-24 19:12:42 +0800

    fix bug

--- corgi-user-service/src/main/java/com/corgi/service/impl/CorgiSystemMessageServiceImpl.java
+++ corgi-user-service/src/main/java/com/corgi/service/impl/CorgiSystemMessageServiceImpl.java
@@ -61,7 +61,7 @@ public class CorgiSystemMessageServiceImpl implements CorgiSystemMessageService 
 
     @Override
     public void updateSystemMessage(SystemMessage systemMessage) {
-        if (!StringUtils.isEmpty(systemMessage.getContent()) || !StringUtils.isEmpty(systemMessage.getStatus()) || systemMessage.getSentTime() != null) {
+        if (!StringUtils.isEmpty(systemMessage.getContent()) || !StringUtils.isEmpty(systemMessage.getStatus()) || systemMessage.getSentTime() != null ||!StringUtils.isEmpty(systemMessage.getTitle())) {
             corgiSystemMessageMapper.updateSystemMessage(systemMessage);
         } else {
             corgiSystemMessageMapper.deleteMessageRule(systemMessage.getId());
