af39ddae36feb2613f7e84ee3a09ea8a619bda57
tairanliu <dasltr@126.com>
2020-07-30 13:26:35 +0800

    fix bug

--- corgi-user-api/src/main/java/com/corgi/user/entity/UserPic.java
+++ corgi-user-api/src/main/java/com/corgi/user/entity/UserPic.java
@@ -14,9 +14,10 @@ public class UserPic extends CorgiPic {
 
     @Override
     public String getPicUrl() {
-        if ("check".equals(super.getStatus()) && super.getPicUrl() != null && !super.getPicUrl().contains("?x-oss-process")) {
+        System.out.println("status ..." + this.getStatus());
+        if ("check".equals(this.getStatus()) && super.getPicUrl() != null && !super.getPicUrl().contains("?x-oss-process")) {
             return super.getPicUrl() + "?x-oss-process=style/mask";
-        } else if(super.getPicUrl() != null){
+        } else if (super.getPicUrl() != null) {
             return super.getPicUrl().split("\\?")[0];
         }
         return super.getPicUrl();
