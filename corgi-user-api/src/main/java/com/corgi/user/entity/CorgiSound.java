030c9c94f259f1c8cf4cfd2c77a5296a5f9a567e
tairanliu <dasltr@126.com>
2020-09-03 00:55:48 +0800

    添加榜单相关接口

--- corgi-user-api/src/main/java/com/corgi/user/entity/CorgiSound.java
+++ corgi-user-api/src/main/java/com/corgi/user/entity/CorgiSound.java
@@ -2,11 +2,13 @@ package com.corgi.user.entity;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 /**
  * @author tairanliu
  */
 @Data
-public class CorgiSound {
+public class CorgiSound implements Serializable {
     public static String NORMAL = "normal";
     public static String NEED_CHECK = "check";
     public static String FAIL = "failed";
