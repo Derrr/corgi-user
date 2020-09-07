ddc452d8d1f9e8d9467f2d8400d9da23626a71c0
tairanliu <dasltr@126.com>
2020-09-07 14:14:16 +0800

    添加头像审核相关接口

--- corgi-user-service/src/main/java/com/corgi/service/impl/CorgiUserServiceImpl.java
+++ corgi-user-service/src/main/java/com/corgi/service/impl/CorgiUserServiceImpl.java
@@ -419,7 +419,7 @@ public class CorgiUserServiceImpl implements CorgiUserService {
             return;
         }
         UserDetail userDetail = corgiUserMapper.getUserDetail(userPosition.getUserId());
-        if (CorgiPic.NORMAL.equals(userDetail.getAvatarCheckStatus()) || UserDetail.NO_FACE.equals(userDetail.getAvatarCheckStatus())) {
+        if (!CorgiPic.NEED_CHECK.equals(userDetail.getAvatarCheckStatus())) {
             redisTemplate.opsForGeo().add(geoKey, new Point(userPosition.getLng(), userPosition.getLat()), userPosition.getUserId());
         }
     }
