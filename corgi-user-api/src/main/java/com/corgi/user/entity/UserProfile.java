b1096a5ab3d2a1f6a0ec904832a13ab959669157
tairanliu <dasltr@126.com>
2020-09-08 17:52:58 +0800

    添加音频字段

--- corgi-user-api/src/main/java/com/corgi/user/entity/UserProfile.java
+++ corgi-user-api/src/main/java/com/corgi/user/entity/UserProfile.java
@@ -47,6 +47,7 @@ public class UserProfile implements Serializable {
     private Integer height;
     private String birthday;
     List<UserPic> pics;
+    List<CorgiSound> sounds;
 
     public String getFollowTime() {
         if (followTime != null && !"".equals(followTime)) {
