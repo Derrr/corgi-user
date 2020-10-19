24ef582b50f36ba9a688f0ebb51133a64f0c5401
tairanliu <dasltr@126.com>
2020-10-19 18:03:08 +0800

    添加序列化

--- corgi-user-api/src/main/java/com/corgi/user/entity/UserVideo.java
+++ corgi-user-api/src/main/java/com/corgi/user/entity/UserVideo.java
@@ -3,11 +3,13 @@ package com.corgi.user.entity;
 import com.corgi.entity.CorgiPic;
 import lombok.Data;
 
+import java.io.Serializable;
+
 /**
  * @author tairanliu
  */
 @Data
-public class UserVideo {
+public class UserVideo implements Serializable {
     private String userId;
     private String videoUrl;
     private String videoId;
