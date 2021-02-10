5ae9df32a8eeb859c3dafc20f48cbfe1b310145b
liutairan <liu.tr@belle.com.cn>
2021-02-10 10:46:34 +0800

    添加城市参数

--- corgi-user-service/src/main/java/com/corgi/service/impl/CorgiDateServiceImpl.java
+++ corgi-user-service/src/main/java/com/corgi/service/impl/CorgiDateServiceImpl.java
@@ -1,6 +1,7 @@
 package com.corgi.service.impl;
 
 import com.alibaba.dubbo.config.annotation.Service;
+import com.corgi.mapper.CorgiDateMapper;
 import com.corgi.user.api.CorgiUserDateService;
 import com.corgi.user.entity.CorgiDate;
 import lombok.extern.slf4j.Slf4j;
@@ -16,24 +17,26 @@ import java.util.List;
 @Slf4j
 @Component
 public class CorgiDateServiceImpl implements CorgiUserDateService {
+    @Autowired
+    private CorgiDateMapper corgiDateMapper;
 
     @Override
     public void addDate(CorgiDate date) {
-
+        corgiDateMapper.addCorgiDate(date);
     }
 
     @Override
     public List<CorgiDate> searchDate(CorgiDate date) {
-        return null;
+        return corgiDateMapper.searchDate(date);
     }
 
     @Override
     public void updateDate(CorgiDate date) {
-
+        corgiDateMapper.updateCorgiDate(date);
     }
 
     @Override
     public CorgiDate getDateById(Integer id) {
-        return null;
+        return corgiDateMapper.getDateById(id);
     }
 }
