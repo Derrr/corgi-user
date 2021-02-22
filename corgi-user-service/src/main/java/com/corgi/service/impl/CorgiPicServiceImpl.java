7df5b0b2894728fac191b8ced7c8d59a19a56aa0
liutairan <liu.tr@belle.com.cn>
2021-02-22 18:55:37 +0800

    fix bug

--- corgi-user-service/src/main/java/com/corgi/service/impl/CorgiPicServiceImpl.java
+++ corgi-user-service/src/main/java/com/corgi/service/impl/CorgiPicServiceImpl.java
@@ -16,7 +16,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
-import sun.swing.StringUIClientPropertyKey;
 
 import java.util.List;
 
