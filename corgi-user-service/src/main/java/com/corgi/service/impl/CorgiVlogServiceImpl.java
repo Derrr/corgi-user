9d117db271961fd648b177fca3886abe98a38bc2
liutairan <liu.tr@belle.com.cn>
2021-03-06 16:49:29 +0800

    优化召回

--- corgi-user-service/src/main/java/com/corgi/service/impl/CorgiVlogServiceImpl.java
+++ corgi-user-service/src/main/java/com/corgi/service/impl/CorgiVlogServiceImpl.java
@@ -53,7 +53,7 @@ public class CorgiVlogServiceImpl implements CorgiVlogService {
 
     @Override
     public List<CorgiVlog> recallVlog(CorgiVlog corgiVlog, Integer limit) {
-        if("like".equals(corgiVlog.getType())){
+        if ("like".equals(corgiVlog.getType()) || "follow".equals(corgiVlog.getType())) {
             return corgiVlogMapper.recallLikeVlog(corgiVlog, limit, UserUtils.getIndex(corgiVlog.getUserId()));
         }
         return corgiVlogMapper.recallVlog(corgiVlog, limit, UserUtils.getIndex(corgiVlog.getUserId()));
