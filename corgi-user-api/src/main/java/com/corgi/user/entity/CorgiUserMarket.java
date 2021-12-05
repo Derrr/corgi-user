7613d73c29d272047c4618724d84d124fe759370
liutairan <liu.tr@belle.com.cn>
2021-12-05 12:01:52 +0800

    添加相关接口

--- corgi-user-api/src/main/java/com/corgi/user/entity/CorgiUserMarket.java
+++ corgi-user-api/src/main/java/com/corgi/user/entity/CorgiUserMarket.java
@@ -1,12 +1,16 @@
 package com.corgi.user.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CorgiUserMarket implements Serializable {
 
     private String id;
