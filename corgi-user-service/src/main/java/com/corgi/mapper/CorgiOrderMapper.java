6c72b5a37a0ee81f7cebd1b055da725719ed2dd7
liutairan <liu.tr@belle.com.cn>
2021-12-09 16:55:58 +0800

    添加相关接口

--- corgi-user-service/src/main/java/com/corgi/mapper/CorgiOrderMapper.java
+++ corgi-user-service/src/main/java/com/corgi/mapper/CorgiOrderMapper.java
@@ -44,5 +44,5 @@ public interface CorgiOrderMapper {
 
     CorgiUserMarket getMarketById(@Param("marketId") String marketId);
 
-    Double sumOrder(@Param("goods") CorgiOrder goods);
+    Double sumOrder(@Param("order") CorgiOrder order);
 }
