ac2010b7c4d9123a6097795a4baf9eb63eb55976
liutairan <liu.tr@belle.com.cn>
2022-01-05 17:24:45 +0800

    添加相关接口

--- corgi-user-service/src/main/java/com/corgi/service/impl/CorgiOrderServiceImpl.java
+++ corgi-user-service/src/main/java/com/corgi/service/impl/CorgiOrderServiceImpl.java
@@ -1,6 +1,7 @@
 package com.corgi.service.impl;
 
 import com.alibaba.dubbo.common.utils.CollectionUtils;
+import com.alibaba.dubbo.common.utils.StringUtils;
 import com.alibaba.dubbo.config.annotation.Service;
 import com.corgi.mapper.CorgiOrderMapper;
 import com.corgi.mapper.CorgiUserMapper;
@@ -180,11 +181,15 @@ public class CorgiOrderServiceImpl implements CorgiOrderService {
     @Override
     public void subscribe(CorgiOrder order, CorgiUserGoods goods, String vipStatus, String finalDate) {
         if ("1".equals(vipStatus)) {
-            corgiOrderMapper.addOrder(order);
-            corgiOrderMapper.updateOrder(order);
-            corgiOrderMapper.addLog(order);
+            if (!StringUtils.isEmpty(order.getTradeNo())) {
+                corgiOrderMapper.addOrder(order);
+                corgiOrderMapper.updateOrder(order);
+                corgiOrderMapper.addLog(order);
+            }
             if (CorgiOrder.STATUS.SUCCESS.equals(order.getStatus())) {
-                corgiOrderMapper.addGoods(goods);
+                if (goods != null) {
+                    corgiOrderMapper.addGoods(goods);
+                }
                 corgiUserMapper.updateVipExpire(order.getUserId(), "1", finalDate);
             }
         } else if ("0".equals(vipStatus)) {
