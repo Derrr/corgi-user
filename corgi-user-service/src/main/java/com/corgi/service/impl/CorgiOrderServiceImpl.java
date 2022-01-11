458df8c3af9ba66fab845753ceb7905e6df7531b
liutairan <liu.tr@belle.com.cn>
2022-01-11 17:00:04 +0800

    添加相关接口

--- corgi-user-service/src/main/java/com/corgi/service/impl/CorgiOrderServiceImpl.java
+++ corgi-user-service/src/main/java/com/corgi/service/impl/CorgiOrderServiceImpl.java
@@ -252,9 +252,9 @@ public class CorgiOrderServiceImpl implements CorgiOrderService {
         JSONArray content = new JSONArray();
         content.add(new JSONObject().fluentPut("text", "Corgi会员服务开通成功通知\n恭喜您已开通 " + days + "天会员服务，目前有效期至" + finalDate + "\n更多"));
         content.add(new JSONObject().fluentPut("text", "会员权益可前往会员页面查看 >").fluentPut("urlType", "9"));
-        HashMap<String, String> extra = new HashMap<>();
+        HashMap<String, Object> extra = new HashMap<>();
         extra.put("type", "907");
-        extra.put("content", content.toJSONString());
+        extra.put("content", content);
         pushMessage.setExtra(extra);
         return pushMessage;
     }
