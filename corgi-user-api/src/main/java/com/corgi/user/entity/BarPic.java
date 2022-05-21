58418176b33eed40b881a15d813588ee5ebdcc7d
liutairan <liu.tr@belle.com.cn>
2022-05-21 19:18:50 +0800

    添加相关接口

--- corgi-user-api/src/main/java/com/corgi/user/entity/BarPic.java
+++ corgi-user-api/src/main/java/com/corgi/user/entity/BarPic.java
@@ -1,13 +1,14 @@
 package com.corgi.user.entity;
 
-import com.corgi.entity.CorgiPic;
 import lombok.Data;
+
+import java.io.Serializable;
 
 /**
  * @author tairanliu
  */
 @Data
-public class BarPic {
+public class BarPic implements Serializable {
     private String picId;
     private String picUrl;
     private String barId;
