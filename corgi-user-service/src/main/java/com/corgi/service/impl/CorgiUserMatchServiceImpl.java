0c097c9481e5b7de3650a29ed86baa2280200c7e
liutairan <liu.tr@belle.com.cn>
2022-06-09 23:45:40 +0800

    添加相关接口

--- corgi-user-service/src/main/java/com/corgi/service/impl/CorgiUserMatchServiceImpl.java
+++ corgi-user-service/src/main/java/com/corgi/service/impl/CorgiUserMatchServiceImpl.java
@@ -330,12 +330,12 @@ public class CorgiUserMatchServiceImpl implements CorgiUserMatchService {
         if (!CollectionUtils.isEmpty(query.getRole())) {
             sb.append(" and d.role in('").append(String.join("','", query.getRole())).append("') ");
         }
-        if (!StringUtils.isEmpty(query.getStartAge())) {
+        if (query.getStartAge() != null && query.getStartAge() > 18) {
             Calendar calendar = Calendar.getInstance();
             calendar.add(Calendar.YEAR, -1 * query.getStartAge());
             sb.append(" and d.birthday < '").append(sdf.format(calendar.getTime())).append("' ");
         }
-        if (!StringUtils.isEmpty(query.getEndAge())) {
+        if (query.getEndAge() != null && query.getEndAge() < 70) {
             Calendar calendar = Calendar.getInstance();
             calendar.add(Calendar.YEAR, -1 * query.getEndAge());
             sb.append(" and d.birthday > '").append(sdf.format(calendar.getTime())).append("' ");
