2d7012398f6a9b5bba19416425f18b67dc71014e
liutairan <liu.tr@belle.com.cn>
2022-06-22 22:03:56 +0800

    添加相关接口

--- corgi-user-api/src/main/java/com/corgi/user/entity/UserProfile.java
+++ corgi-user-api/src/main/java/com/corgi/user/entity/UserProfile.java
@@ -48,7 +48,6 @@ public class UserProfile implements Serializable {
     private Integer height;
     private String birthday;
     private String city;
-    private Boolean vip;
     List<UserPic> pics;
     CorgiSound sounds;
 
