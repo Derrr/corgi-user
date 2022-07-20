1c4f3d3667ea889dff51a38339497de2aac91e63
liutairan <liu.tr@belle.com.cn>
2022-07-20 16:33:13 +0800

    添加相关接口

--- corgi-user-service/src/main/java/com/corgi/service/impl/CorgiOrderServiceImpl.java
+++ corgi-user-service/src/main/java/com/corgi/service/impl/CorgiOrderServiceImpl.java
@@ -75,7 +75,19 @@ public class CorgiOrderServiceImpl implements CorgiOrderService {
                 expiresDate = order.getBuyerId();
                 order.setBuyerId(null);
             }
+            if (StringUtils.isNotEmpty(order.getOrderId())) {
+                List<CorgiOrder> oldOrders = corgiOrderMapper.getOrderByOrderId(order.getOrderId());
+                if (CollectionUtils.isNotEmpty(oldOrders)) {
+                    for (CorgiOrder oldOrder : oldOrders) {
+                        if (CorgiOrder.STATUS.SUCCESS.equals(oldOrder.getStatus())
+                                && oldOrder.getUserId().equals(order.getUserId())) {
+                            order.setStatus("duplicated");
+                        }
+                    }
+                }
+            }
             corgiOrderMapper.updateOrder(order);
+
             if (CorgiOrder.STATUS.SUCCESS.equals(order.getStatus())) {
                 this.buy(order.getTradeNo(), expiresDate);
             }
