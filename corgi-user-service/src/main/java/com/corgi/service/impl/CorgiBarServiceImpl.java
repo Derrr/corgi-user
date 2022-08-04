2f81365be7d5884b779add91b9358c32d48ad95d
liutairan <liu.tr@belle.com.cn>
2022-08-05 01:47:19 +0800

    添加相关接口

--- corgi-user-service/src/main/java/com/corgi/service/impl/CorgiBarServiceImpl.java
+++ corgi-user-service/src/main/java/com/corgi/service/impl/CorgiBarServiceImpl.java
@@ -49,7 +49,7 @@ public class CorgiBarServiceImpl implements CorgiBarService {
     @Autowired
     private CorgiVideoService corgiVideoService;
     @Autowired
-    private RedisTemplate<String, Object> redisTemplate;
+    private StringRedisTemplate redisTemplate;
 
     @Reference
     private CorgiActivityService corgiActivityService;
@@ -117,7 +117,7 @@ public class CorgiBarServiceImpl implements CorgiBarService {
             Object totalCountObj = redisTemplate.opsForValue().get(key);
             if (totalCountObj == null) {
                 totalCount = (int) corgiActivityService.countCorgiActivity(corgiActivity);
-                redisTemplate.opsForValue().set(key, totalCount, 1l, TimeUnit.HOURS);
+                redisTemplate.opsForValue().set(key, totalCount + "", 1l, TimeUnit.HOURS);
             } else {
                 try {
                     totalCount = Integer.valueOf(totalCountObj.toString());
