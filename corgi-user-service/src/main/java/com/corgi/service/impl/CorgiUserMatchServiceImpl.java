508865c943f8c4776c72c42cdf6426a9767a61fe
liutairan <liu.tr@belle.com.cn>
2022-08-17 13:56:01 +0800

    添加相关接口

--- corgi-user-service/src/main/java/com/corgi/service/impl/CorgiUserMatchServiceImpl.java
+++ corgi-user-service/src/main/java/com/corgi/service/impl/CorgiUserMatchServiceImpl.java
@@ -84,12 +84,8 @@ public class CorgiUserMatchServiceImpl implements CorgiUserMatchService {
             userIds.add(item.getUserId());
         }
         String key = "user_match_view_" + dateStr + userQuery.getUserId();
-        try {
-            redisTemplate.opsForSet().add(key, (String[]) userIds.toArray());
-            redisTemplate.expire(key, 1l, TimeUnit.DAYS);
-        } catch (Exception e) {
-            redisTemplate.delete(key);
-        }
+        redisTemplate.opsForSet().add(key, (String[]) userIds.toArray());
+        redisTemplate.expire(key, 1l, TimeUnit.DAYS);
         return users;
     }
 
@@ -176,15 +172,11 @@ public class CorgiUserMatchServiceImpl implements CorgiUserMatchService {
     public void addUserMatch(String userId, String matchId, String tradeNo) {
         userMatchMapper.addMatch(userId, matchId, tradeNo);
         String key = "user_match_" + userId;
-        try {
-            if (redisTemplate.hasKey(key)) {
-                redisTemplate.opsForSet().add(key, matchId + "-" + System.currentTimeMillis());
-            } else {
-                redisTemplate.opsForSet().add(key, matchId + "-" + System.currentTimeMillis());
-                redisTemplate.expire(key, 14l, TimeUnit.DAYS);
-            }
-        } catch (Exception e) {
-            redisTemplate.delete(key);
+        if (redisTemplate.hasKey(key)) {
+            redisTemplate.opsForSet().add(key, matchId + "-" + System.currentTimeMillis());
+        } else {
+            redisTemplate.opsForSet().add(key, matchId + "-" + System.currentTimeMillis());
+            redisTemplate.expire(key, 14l, TimeUnit.DAYS);
         }
     }
 
