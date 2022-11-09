3fc93a3412032f4b0023b334f821fa4fea170331
trliu <trliu@hillinsight.com>
2022-11-09 12:18:40 +0800

    添加相关接口

--- corgi-user-service/src/main/java/com/corgi/service/impl/CorgiVlogServiceImpl.java
+++ corgi-user-service/src/main/java/com/corgi/service/impl/CorgiVlogServiceImpl.java
@@ -13,10 +13,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
-import org.springframework.util.CollectionUtils;
 
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
