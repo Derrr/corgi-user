a526881396b56b4df9f3129d1bdedaa6c286826f
trliu <trliu@hillinsight.com>
2022-12-06 11:33:49 +0800

    添加付费榜单推送

--- corgi-user-api/src/main/java/com/corgi/user/entity/TopicBillboard.java
+++ corgi-user-api/src/main/java/com/corgi/user/entity/TopicBillboard.java
@@ -6,6 +6,7 @@ import java.io.Serializable;
 
 @Data
 public class TopicBillboard implements Serializable {
+    public static final String PREFIX = "topic_most_popular_";
     private String activityId;
     private String topic;
     private Integer status;
