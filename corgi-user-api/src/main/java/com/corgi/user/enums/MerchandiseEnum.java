68993ac7660a745fbb85096d20613197afdc21dd
trliu <trliu@hillinsight.com>
2024-03-20 11:24:25 +0800

    添加微信相关接口

--- corgi-user-api/src/main/java/com/corgi/user/enums/MerchandiseEnum.java
+++ corgi-user-api/src/main/java/com/corgi/user/enums/MerchandiseEnum.java
@@ -11,6 +11,7 @@ public enum MerchandiseEnum {
     HALF_YEAR("S06", "normal", 181, "VIP/半年 ¥249", "连续包半年"),
     SEASON("S07", "normal", 90, "VIP/季 ¥124", "连续包季"),
     MONTH("S08", "normal", 30, "VIP/月 ¥41", "连续包月"),
+    BONUS("BS01", "normal", 7, "免费", "奖励VIP"),
     READ_1("A01", "normal", 0, "付费阅读 ¥1", "付费阅读"),
     READ_3("A02", "normal", 0, "付费阅读 ¥3", "付费阅读"),
     READ_6("A03", "normal", 0, "付费阅读 ¥6", "付费阅读"),
