72e43e3d3fac06e2b466c64e1ea5c5b23cd470fb
trliu <trliu@hillinsight.com>
2024-04-30 16:54:04 +0800

    添加邀请相关接口

--- corgi-user-service/src/main/java/com/corgi/service/impl/CorgiUserMatchServiceImpl.java
+++ corgi-user-service/src/main/java/com/corgi/service/impl/CorgiUserMatchServiceImpl.java
@@ -127,18 +127,6 @@ public class CorgiUserMatchServiceImpl implements CorgiUserMatchService {
         String key = "user_match_" + userId;
         try {
             if (redisTemplate.hasKey(key)) {
-                String threshold = (System.currentTimeMillis() - 14 * 24 * 3600 * 1000) + "";
-                List<String> oldMatches = redisTemplate.opsForList().range(key, 0, -1);
-                for (String oldMatch : oldMatches) {
-                    if (!StringUtils.isEmpty(oldMatch) && oldMatch.split("-").length > 1) {
-                        String addTime = oldMatch.split("-")[1];
-                        if (threshold.compareTo(addTime) > 0) {
-                            redisTemplate.opsForList().leftPop(key);
-                        } else {
-                            break;
-                        }
-                    }
-                }
                 redisTemplate.opsForList().rightPush(key, matchId + "-" + System.currentTimeMillis());
             } else {
                 redisTemplate.opsForList().rightPush(key, matchId + "-" + System.currentTimeMillis());
