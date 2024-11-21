c8ccba0c96a58132e674176066b1918c000b2535
trliu <trliu@hillinsight.com>
2024-11-21 11:01:54 +0800

    添加天狼星活动相关接口

--- corgi-user-service/src/main/java/com/corgi/service/impl/CorgiSystemMessageServiceImpl.java
+++ corgi-user-service/src/main/java/com/corgi/service/impl/CorgiSystemMessageServiceImpl.java
@@ -113,7 +113,7 @@ public class CorgiSystemMessageServiceImpl implements CorgiSystemMessageService 
 
     @Override
     public Integer countMessageRecordByMessageId(String messageId) {
-        return corgiSystemMessageMapper.countSystemMessage(messageId);
+        return corgiSystemMessageMapper.countMessageRecordByMessageId(messageId);
     }
 
     @Override
